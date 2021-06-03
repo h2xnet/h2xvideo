27997950009a5d10d6ce70716a6fa5ae85145cf4
xingguangzfs <fushou3579@163.com>
2021-06-03 23:02:52 +0800

    add h2x_listen

---
+++ h2xcore/h2x_listen.h
@@ -0,0 +1,23 @@
+#ifndef H2XCORE_H2X_LISTEN__H
+#define H2XCORE_H2X_LISTEN__H
+
+#include "h2xcore/h2x_core_export.h"
+
+namespace h2x {
+
+/*
+* ClassName: Listen
+* Desc:º‡Ã˝¿‡
+* Author: zhaofushou
+* Date: 2021-06-03 22:51
+*/
+class H2XCORE_EXPORT Listen {
+public:
+    Listen();
+    virtual ~Listen();
+
+};
+
+} // end namespace h2x
+
+#endif // !H2XCORE_H2X_LISTEN__H
